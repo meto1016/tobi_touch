1218b8f3b837c4cf07bd13228cc6e14aa892dbf1
meto1015 <tobimetzger@Glumanda.local>
2015-11-20 03:12:27 +0100

    message box + email

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 Ext.application({
     name : 'Fiddle',
 
+	requires: [
+        'Ext.MessageBox'
+    ],
+
     launch : function() {
+		
 
         var store = Ext.create('Ext.data.Store', {
             autoLoad: true,
-            fields: ['entryId', 'firstName'],
+            fields: ['entryId', 'firstName','email'],
             proxy: {
                 type: 'ajax',
                 url: 'data.xml',
@@ -16,13 +21,23 @@ Ext.application({
                 }
             }            
         });
+		
 
         Ext.create('Ext.DataView', {
             fullscreen: true,
             store: store,
-            itemTpl: '<h2>{entryId}</h2> <p>{firstName}</p>'
+            itemTpl: '<h2>{entryId}</h2> <p>{firstName}</p> <p>{email}</p>'
         });
-    }
+		
+		Ext.Msg.alert (
+								 	'Hello!',
+									'Hello there from Tobi Touch!'
+									
+						);
+		
+		
+    },
+
 }); test
 
  
