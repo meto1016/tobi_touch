b8aee0e5ec73c2b018de665ec8a3b88e07f8ceac
meto1016 <meto1016@hs-karlsruhe.de>
2015-12-06 17:34:37 +0100

    style update topweine & perlweine

--- app/controller/Topcontroller.js
+++ app/controller/Topcontroller.js
@@ -13,29 +13,36 @@ Ext.define('app.controller.Topcontroller', {
             
         }
     },
+	
 	showPost: function(list, index, element, record) {
 			/*console.log(record.get('info'));*/
 			this.getBlog().push({
 							xtype: 'panel',
 							title: record.get('name'),
-                     style: {
+                            style: {
                         backgroundImage: 'url(resources/img/wein_background_2.jpg)',
                         backgroundRepeat: 'no-repeat',
                         backgroundPosition: 'center'
-                     },     
-                    html: [
-                        '<div class="central">',    
-                            "<h2>", record.get('name'), "</h2>",
-                            '<p><img class="detail-img" src="', record.get('url'), '"/> <div class="rating-conotroller"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div></p>',
-                        '<span >',
-                            '<p class="border-info">', record.get('detail-info'), '<p>',
-                        '</span>',
-                        '<div>'
-                          ].
-                            join(""),
-                    scrollable: true,
-                    styleHtmlContent: true
-                        });
+                     },
+							html: ["<table >",
+                                  " <tr>",
+                                   "<td>",
+                                   "<h3 style='color:#AF1E2D'>", record.get('name'), "</h3>",
+                                   "</td><td></td></tr>",
+                                   "<tr><td><b>Jahrgang:</b>&nbsp", record.get('jahr'),"</p>",
+                                   "<p><b>Winzer:</b>&nbsp", record.get('winzer'),"</p>",
+                                   "<p><b>Farbe/Typus:</b>&nbsp", record.get('farbe'), "</p>",
+                                   "<p><b>Alkoholgehalt:</b>&nbsp", record.get('alk'),"</p>",
+                                   "<p><b>Geschmack:</b>&nbsp", record.get('geschmack'),"</p></td>",
+                                   "<td>",
+                                  '<img src="', record.get('url'), '"/>',
+                                   "</td></tr>",
+                                  "</table>",
+                                  "<h3 style='color:#AF1E2D'><b>Preis:", record.get('preis'),"</b></h3>"].
+                                    join(""),
+							scrollable: true,
+							styleHtmlContent: true
+								});
 		},
     
     //called when the Application is launched, remove if not needed
--- app/view/Top.js
+++ app/view/Top.js
@@ -1,19 +1,18 @@
 Ext.define('app.view.Top',{
-		    extend: 'Ext.navigation.View',
+		  extend: 'Ext.navigation.View',
 			xtype: 'toppanel',
-            
+			
 			config: {
 				title: 'Top 10',
 				iconCls: 'home',
-				html: 'Topweintest',
+				
 				items: {
 					xtype: 'list',
-					itemTpl: '<p><img src="{url}", height="125px",width="125px"/> &nbsp; <h3>{name}</h3><div class="rating"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div></p>',
-                    
+					itemTpl:'<table><tr><td><img src="{url}", height="125px",width="125px"/></td><td><h3 style="color:#AF1E2D">{name}</h3></td></tr><tr><td><div class="rating"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div></td><td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPreis:&nbsp{preis}</td></tr></table>',
 					
 					store: {
 						autoLoad: true,
-						fields: ['name', 'id', 'info', 'url', 'detail-info', 'bewertung', 'test'],
+						fields: ['name', 'id', 'jahr', 'winzer', 'farbe', 'rebsorte', 'alk', 'geschmack', 'url', 'preis', 'detail-info', 'bewertung' ],
 						
 						 proxy: {
 							type: 'ajax',
@@ -25,9 +24,8 @@ Ext.define('app.view.Top',{
 								record: 'wein'
 						}
 						}
-					}
+					}           
 					   
 			}
-}
-		   
+} 
 });
