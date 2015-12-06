04f0925316c6e652d47fe886354cac4da5ecd5d1
meto1016 <meto1016@hs-karlsruhe.de>
2015-12-06 16:35:19 +0100

    weißwein style update

--- app/controller/Weisscontroller.js
+++ app/controller/Weisscontroller.js
@@ -3,8 +3,7 @@ Ext.define('app.controller.Weisscontroller', {
     
     config: {
         refs: {
-			blog: 'weisspanel',
-           
+			blog: 'weisspanel'
             
         },
         control: {
@@ -20,9 +19,26 @@ Ext.define('app.controller.Weisscontroller', {
 			this.getBlog().push({
 							xtype: 'panel',
 							title: record.get('name'),
-                            
-							html: ["<h2 style='color:white'>", record.get('name'), "</h2>",
-                                  '<img src="', record.get('url'), '"/>'].
+                            style: {
+                        backgroundImage: 'url(resources/img/wein_background_2.jpg)',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center'
+                     },
+							html: ["<table >",
+                                  " <tr>",
+                                   "<td>",
+                                   "<h3 style='color:#AF1E2D'>", record.get('name'), "</h3>",
+                                   "</td><td></td></tr>",
+                                   "<tr><td><b>Jahrgang:</b>&nbsp", record.get('jahr'),"</p>",
+                                   "<p><b>Winzer:</b>&nbsp", record.get('winzer'),"</p>",
+                                   "<p><b>Farbe/Typus:</b>&nbsp", record.get('farbe'), "</p>",
+                                   "<p><b>Alkoholgehalt:</b>&nbsp", record.get('alk'),"</p>",
+                                   "<p><b>Geschmack:</b>&nbsp", record.get('geschmack'),"</p></td>",
+                                   "<td>",
+                                  '<img src="', record.get('url'), '"/>',
+                                   "</td></tr>",
+                                  "</table>",
+                                  "<h3 style='color:#AF1E2D'><b>Preis:", record.get('preis'),"</b></h3>"].
                                     join(""),
 							scrollable: true,
 							styleHtmlContent: true
--- app/view/Weiss.js
+++ app/view/Weiss.js
@@ -1,5 +1,5 @@
 Ext.define('app.view.Weiss',{
-		   extend: 'Ext.navigation.View',
+		  extend: 'Ext.navigation.View',
 			xtype: 'weisspanel',
 			
 			config: {
@@ -8,11 +8,11 @@ Ext.define('app.view.Weiss',{
 				
 				items: {
 					xtype: 'list',
-					itemTpl:'{name}, {id}',
+					itemTpl:'<table><tr><td><img src="{url}", height="125px",width="125px"/></td><td><h3 style="color:#AF1E2D">{name}</h3></td></tr><tr><td><div class="rating"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></div></td><td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspPreis:&nbsp{preis}</td></tr></table>',
 					
 					store: {
 						autoLoad: true,
-						fields: ['name', 'id', 'bewertung'],
+						fields: ['name', 'id', 'jahr', 'winzer', 'farbe', 'rebsorte', 'alk', 'geschmack', 'url', 'preis', 'detail-info', 'bewertung' ],
 						
 						 proxy: {
 							type: 'ajax',
@@ -27,5 +27,5 @@ Ext.define('app.view.Weiss',{
 					}           
 					   
 			}
-}   
+} 
 });
